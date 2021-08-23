918c0934b5be34c93272fa4480b9bc74eeaf2b4e
Nicholas Ciechanowski <NicholasACiechanowski@gmail.com>
2021-08-23 16:05:18 +1000

    Updated to return sooner instead of nested if's

--- modules/widgets/synergywholesale_servercost.php
+++ modules/widgets/synergywholesale_servercost.php
@@ -79,64 +79,82 @@ class CostWidget extends AbstractWidget
             ->where('name', 'Synergy Wholesale')
             ->first();
 
-        // Check if SWS server is found
+        // Return error if it can't find the server.
         if (empty($query)) {
             return "<div style='text-align: center; color: red; font-weight: bold; font-size: 18px; padding: 16px;'> Failed to get Synergy Server Monthly Cost</div>";
         }
 
-        // Check if button is clicked
-        if ($syncCost) {
-            // Check if details are in the module.
-            if (!empty($result['api_key']) && !empty($result['reseller_id'])) {
-                $api = new CostWidgetAPI($result['reseller_id'], $result['api_key']);
-                $hostingServices = $api->listHosting();
-
-                // Check if it was able to get hosting list
-                if ($hostingServices->status == "OK") {
-                    // Collect and get 'active' services
-                    $services = collect($hostingServices->hoidList);
-                    $activeServicesPlans = $services->whereIn('serviceStatus', ['Active', 'Suspended', 'Suspended By Staff', 'Pending Completion', 'Pending Upgrade'])->countBy('plan');
-
-                    $hostingPlans = $api->hostingListPackages();
-
-                    // Check if it was abel to get plan list
-                    if ($hostingPlans->status == "OK") {
-                        $plans = collect($hostingPlans->packages);
-                        $cost = '0.00';
-
-                        // loop collected services plans and get plan cost and count it up
-                        foreach ($activeServicesPlans as $key => $value) {
-                            $planData = $plans->where('name', $key)->first();
-
-                            $cost = bcadd($cost, bcmul($planData->price, $value, 2), 2);
-                        }
-
-                        // No model for this yet, so we'll need to update it like this and refetch it, like this.
-                        // Add new cost into the database
-                        DB::table('tblservers')
-                            ->where('name', 'Synergy Wholesale')
-                            ->update(['monthlycost' => $cost]);
-
-                        // Refetch it after we've updated
-                        $query = DB::table('tblservers')
-                            ->where('name', 'Synergy Wholesale')
-                            ->first();
-
-                        $content .= "<div style='text-align: center; color: green; font-weight: bold; font-size: 12px; padding: 16px;'> Successfully synced server cost.</div>";
-                    } else {
-                        $content .= "<div style='text-align: center; color: red; font-weight: bold; font-size: 12px; padding: 16px;'>{$hostingPlans->errorMessage}</div>";
-                    }
-                } else {
-                    $content .= "<div style='text-align: center; color: red; font-weight: bold; font-size: 12px; padding: 16px;'>{$hostingServices->errorMessage}</div>";
-                }
-            } else {
-                $content .= "<div style='text-align: center; color: red; font-weight: bold; font-size: 12px; padding: 16px;'> Unable to find Synergy Wholesale details, check product module settings.</div>";
-            }
+        // If button wasn't clicked just return current data from the DB
+        if (empty($syncCost)) {
+            return $this->getDefaultContent($query);
         }
 
-        $content .= "<div style='text-align: center; color: green; font-weight: bold; font-size: 18px; padding: 8px;'>Monthly Cost: $" . number_format($query->monthlycost, 2) . "</div>";
-        $content .= "<div style='text-align: center; padding: 8px;'> <button type='button' id='sync_cost' class='btn btn-sm btn-info'>Sync Cost</button></div>";
-        $content .= "
+        // Return error if details are missing the module.
+        if (empty($result['api_key']) || empty($result['reseller_id'])) {
+            $content .= "<div style='text-align: center; color: red; font-weight: bold; font-size: 12px; padding: 16px;'> Unable to find Synergy Wholesale details, check product module settings.</div>";
+            $content .= $this->getDefaultContent($query);
+            return $content;
+        }
+
+        // connect to API and get hosting services.
+        $api = new CostWidgetAPI($result['reseller_id'], $result['api_key']);
+
+        $hostingServices = $api->listHosting();
+
+        // Check if it was able to get hosting list otherwise return error
+        if ($hostingServices->status != "OK") {
+            $content .= "<div style='text-align: center; color: red; font-weight: bold; font-size: 12px; padding: 16px;'>{$hostingServices->errorMessage}</div>";
+            $content .= $this->getDefaultContent($query);
+            return $content;
+        }
+
+        // Collect and get 'active' services
+        $services = collect($hostingServices->hoidList);
+        $activeServicesPlans = $services->whereIn('serviceStatus', ['Active', 'Suspended', 'Suspended By Staff', 'Pending Completion', 'Pending Upgrade'])->countBy('plan');
+
+        $hostingPlans = $api->hostingListPackages();
+
+        // Check if it was abel to get plan list otherwise return error
+        if ($hostingPlans->status != "OK") {
+            $content .= "<div style='text-align: center; color: red; font-weight: bold; font-size: 12px; padding: 16px;'>{$hostingPlans->errorMessage}</div>";
+            $content .= $this->getDefaultContent($query);
+            return $content;
+        }
+
+        $plans = collect($hostingPlans->packages);
+        $cost = '0.00';
+
+        // loop collected services plans and get plan cost and count it up
+        foreach ($activeServicesPlans as $key => $value) {
+            $planData = $plans->where('name', $key)->first();
+
+            $cost = bcadd($cost, bcmul($planData->price, $value, 2), 2);
+        }
+
+        // No model for this yet, so we'll need to update it like this and refetch it, like this.
+        // Add new cost into the database
+        DB::table('tblservers')
+            ->where('name', 'Synergy Wholesale')
+            ->update(['monthlycost' => $cost]);
+
+        $content .= "<div style='text-align: center; color: green; font-weight: bold; font-size: 12px; padding: 16px;'> Successfully synced server cost.</div>";
+        $content .= $this->getDefaultContent();
+
+        return $content;
+    }
+
+    public function getDefaultContent($query = '')
+    {
+        // If not passed fetch it again.
+        if (empty($query)) {
+            $query = DB::table('tblservers')
+                ->where('name', 'Synergy Wholesale')
+                ->first();
+        }
+
+        $defaultContent = "<div style='text-align: center; color: green; font-weight: bold; font-size: 18px; padding: 8px;'>Monthly Cost: $" . number_format($query->monthlycost, 2) . "</div>";
+        $defaultContent .= "<div style='text-align: center; padding: 8px;'> <button type='button' id='sync_cost' class='btn btn-sm btn-info'>Sync Cost</button></div>";
+        $defaultContent .= "
                 <script>
                 $(document).ready(function() {
                     $('#sync_cost').click(function() {
@@ -146,7 +164,7 @@ class CostWidget extends AbstractWidget
                 </script>
                 ";
 
-        return $content;
+        return $defaultContent;
     }
 }
 
